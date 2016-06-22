09f95d0c1645c7463f539446c94281790a30f019
Erno Tuovinen <ere.0000@yahoo.com>
2016-06-22 11:26:52 +0000

    Added query params for sign up buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
     def home
-        
+        @basic_plan = Plan.find(1) # ID 1
+        @pro_plan = Plan.find(2) # ID 2
     end
     
     def about
