58958f97196d02e135c1777ba6d8eb3e6f367d0e
Erno Tuovinen <ere.0000@yahoo.com>
2016-07-18 07:41:58 +0000

    Added all kinds stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   
   # Käyttäjän pitää olla joko basic tai pro account
   belongs_to :plan
+  has_one :profile 
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
