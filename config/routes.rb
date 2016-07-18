6b0345ecd2cf1c7ec930ce3c7b85bafa39ed32d7
Erno Tuovinen <ere.0000@yahoo.com>
2016-07-18 09:35:03 +0000

    Profiles controller and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :contacts
   
+  resources :users do 
+    resource :profile
+  end
+  
   get '/about' => 'pages#about'
   root 'pages#home'
   
